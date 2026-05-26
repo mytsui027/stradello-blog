15dface45765dc0f844bb619d8a483137724cedd
Mytsui027 <clebermytsuioficial@gmail.com>
2026-05-26 15:18:55 -0300

    fix tailwind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://stradello-blog.vercel.app',
-  integrations: [],
+site: 'https://stradello-blog.vercel.app',
+integrations: [tailwind()],
 });
