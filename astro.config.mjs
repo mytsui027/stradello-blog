e5e3b137946a9b58ee8c177e33e96a77f1ef083c
Mytsui027 <clebermytsuioficial@gmail.com>
2026-06-08 18:05:40 -0300

    Fix: padronizar URLs para usar https://www.stradello.com.br

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,7 +18,7 @@ const remarkRemoveDuplicateH1 = () => {
 };
 
 export default defineConfig({
-	site: 'https://stradello.com.br',
+	site: 'https://www.stradello.com.br',
 	integrations: [tailwind()],
 	markdown: {
 		remarkPlugins: [remarkRemoveDuplicateH1],
